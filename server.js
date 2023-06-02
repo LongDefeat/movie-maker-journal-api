7e7d39aa01f389fdd165743fa35883c1f78d9b17
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-06-01 19:36:24 -0700

    add port origin

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = require("./app");
 const cors = require("cors");
 
 app.use(cors({ origin: "*" }));
-const serverPort = 3001;
+
+const PORT = process.env.PORT;
 app.listen(serverPort, function () {
-  console.log(`Started on http://localhost:${serverPort}`);
+  console.log(`Started on http://localhost:${PORT}`);
 });
