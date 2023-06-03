5d6106c388cb521f06e74218d3aae4b914b80b91
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-06-02 20:51:19 -0700

    further experimenting with deployment

--- app.js
+++ app.js
@@ -38,16 +38,16 @@ app.use(function (req, res, next) {
   return next(new NotFoundError());
 });
 
-// /** Generic error handler; anything unhandled goes here. */
-// app.use(function (err, req, res, next) {
-//   if (process.env.NODE_ENV !== "test") console.error(err.stack);
-//   const status = err.status || 500;
-//   const message = err.message;
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
 
-//   return res.status(status).json({
-//     error: { message, status },
-//   });
-// });
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("hi");
